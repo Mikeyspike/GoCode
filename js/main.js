1d681dcb7e1e653086cb3c6ed39de9dc85761076
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-01-18 18:24:58 +0000

    Added ACE Editor
    Added the ace editor into the app. incorporated resizing elements to
    span to size of window.
    
    upped the viewport scale to 2

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/html");
+
+
+$(document).ready(function () {
+    changeSize();
+    editor.setValue("<!DOCTYPE html>\n\
+<html>\n\
+<head>\n\
+    <title>GoCode v7</title>\n\
+</head>\n\
+<body>\n\
+    Welcome to GoCode!\n\
+</body>\n\
+</html>");
+});
+
+function changeSize() {
+    var bodyheight = $(window).height();
+    $("#editor").height(bodyheight - 174);
+    $(".ace_content").height(bodyheight - 174);
+    $(window).resize(function () {
+        var bodyheight = $(window).height();
+        $("#editor").height(bodyheight - 174);
+        $(".ace_content").height(bodyheight - 174);
+    });
+}
