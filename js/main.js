8c0c5abdb30ec58905995fd0acce1aa3f8c44fd3
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-02-03 17:36:54 +0000

    File Open Test 1
    Added some stuff to see if i can get it to open a file.

--- js/main.js
+++ js/main.js
@@ -45,18 +45,39 @@ $('#fontsize').on('change', function () {
 });
 
 $('#file-open-button').click(function(){
-    win();
+    init();
 });
 
-function win(file) {
-    var reader = new FileReader();
-    reader.onloadend = function (evt) {
-        console.log("read success");
-        console.log(evt.target.result);
-    };
-    reader.readAsText(file);
-};
+function init() {
+    
+    //This alias is a read-only pointer to the app itself
+    window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
 
-var fail = function (error) {
-    console.log(error.code);
-};
+    /* Yes, this works too for our specific example...
+    $.get("index.html", function(res) {
+        console.log("index.html", res);
+    });
+    */
+
+}
+
+function fail(e) {
+    console.log("FileSystem Error");
+    console.dir(e);
+}
+
+function gotFile(fileEntry) {
+
+    fileEntry.file(function(file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function(e) {
+            console.log("Text is: "+this.result);
+            //document.querySelector("#textArea").innerHTML = this.result;
+            editor.setValue(this.result);
+        }
+
+        reader.readAsText(file);
+    });
+
+}
