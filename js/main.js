88a5c9d1b4da6963d0a4fc1ca6b0fb016553af78
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-02-05 11:28:48 +0000

    File Save Test 1

--- js/main.js
+++ js/main.js
@@ -46,30 +46,52 @@ $('#fontsize').on('change', function () {
 });
 
 $('#file-open-button').click(function(){
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-    window.resolveLocalFileSystemURL("file:///storage/emulated/0/Files/file.txt", onResolveSuccess, fail);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, FileSystemSuccess, fail);
+    window.resolveLocalFileSystemURL("file:///storage/emulated/0/Files/file.txt", obtainFileSuccess, fail);
 });
 
-function onFileSystemSuccess(fileSystem) {
-        console.log("on FileSystem Success");
-        console.log(fileSystem.name);
-    }
-
-    function onResolveSuccess(fileEntry) {
-        console.log("on Resolve Success");
-        console.log(fileEntry.name);
-         fileEntry.file(function(file) {
-            var reader = new FileReader();
-
-            reader.onloadend = function(e) {
-                console.log("Text is: "+this.result);
-                //document.querySelector("#textArea").innerHTML = this.result;
-                editor.setValue(this.result);
-            }
-
-            reader.readAsText(file);
+$('#file-save-button').click(function(){
+    window.resolveLocalFileSystemURL("file:///storage/emulated/0/Files/", function(dir) {
+        console.log("got main dir",dir);
+        dir.getFile("log.txt", {create:true}, function(file) {
+            console.log("got the file", file);
+            logOb = file;
+            var fileContent = editor.getValue();
+            writeLog(fileContent);            
         });
-    }
+    });
+});
+
+function writeLog(str) {
+    if(!logOb) return;
+    var log = str + " [" + (new Date()) + "]\n";
+    console.log("going to log "+log);
+    logOb.createWriter(function(fileWriter) {
+        
+        fileWriter.seek(fileWriter.length);
+        
+        var blob = new Blob([log], {type:'text/plain'});
+        fileWriter.write(blob);
+        console.log("ok, in theory i worked");
+    }, fail);
+}
+
+function FileSystemSuccess(fileSystem) {
+    console.log("Got the filesystem!");
+}
+
+function obtainFileSuccess(fileEntry) {
+    console.log("Obtained File!");
+    console.log(fileEntry.name);
+        fileEntry.file(function(file) {
+            var reader = new FileReader();
+            reader.onloadend = function(e) {
+                console.log("Opened File");
+                editor.setValue(this.result);
+        }
+        reader.readAsText(file);
+    });
+}
 
 function fail(e) {
     console.log("FileSystem Error");
