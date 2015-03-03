00dab7137a40b39b392329f5b6090413e81b20d5
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-03-03 09:02:52 +0000

    Switch to Materialize
    Switched to Materialize for styling.
    re-written javascript.
    removed unneeded code.
    re-written HTML

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/html");
+	editor.setTheme("ace/theme/monokai");
+	editor.getSession().setMode("ace/mode/html");
+
 
 
 $(document).ready(function () {
-    changeSize();
-    editor.setValue("<!DOCTYPE html>\n\
-<html>\n\
-<head>\n\
-    <title>GoCode v7</title>\n\
-</head>\n\
-<body>\n\
-    Welcome to GoCode!\n\
-</body>\n\
-</html>");
+	changeSize();
+    $('.modal-trigger').leanModal();
+    $('select').material_select();
+    $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 100});
 
-    var fileInput = document.getElementById('fileInput');
+    $('#new-file-modal-yes').click(function(){
+        $('.button-collapse').sideNav('hide');
+        toast('New File Created!', 4000) // 4000 is the duration of the toast
+        editor.setValue(null);
+    });
+
+    //App settings changes
+    $('#mode').on('change', function () {
+        var newMode = $("#mode").val()
+        editor.getSession().setMode("ace/mode/"+newMode);
+    });
+    $('#theme').on('change', function () {
+        var newTheme = $("#theme").val();
+        editor.setTheme(newTheme);
+    });
+    $('#fontsize').on('change', function () {
+        var fontSize = $("#fontsize").val();
+        $('#editor').css("font-size", fontSize);
+    });
+
+    //file open
+    var fileInput = document.getElementById('fileinput');
     //var fileDisplayArea = editor.setValue();
 
     fileInput.addEventListener('change', function(e) {
@@ -27,6 +43,9 @@ $(document).ready(function () {
 
             reader.onload = function(e) {
                 editor.setValue(reader.result);
+                toast('File Opened!', 4000) // 4000 is the duration of the toast
+                $('#open-file-modal').closeModal();
+                $('.button-collapse').sideNav('hide');
             }
 
             reader.readAsText(file);    
@@ -34,46 +53,27 @@ $(document).ready(function () {
             editor.setValue("File Not Supported!!!");
         }
     });
-});
 
+});
 
 function changeSize() {
     var bodyheight = $(window).height();
-    $("#editor").height(bodyheight - 101);
-    $(".ace_content").height(bodyheight - 101);
+    $("#editor").height(bodyheight - 109);
+    $(".ace_content").height(bodyheight - 109);
     $(window).resize(function () {
         var bodyheight = $(window).height();
-        $("#editor").height(bodyheight - 101);
-        $(".ace_content").height(bodyheight - 101);
+        $("#editor").height(bodyheight - 109);
+        $(".ace_content").height(bodyheight - 109);
     });
 }
 
-$('#new-file-confirm').click(function(){
-    editor.setValue("");
-});
 
-$('#mode').on('change', function () {
-    var newMode = $("#mode").val();
-    editor.getSession().setMode("ace/mode/"+newMode);
-});
-$('#theme').on('change', function () {
-    var newTheme = $("#theme").val();
-    editor.setTheme(newTheme);
-});
-$('#fontsize').on('change', function () {
-    var fontSize = $("#fontsize").val();
-    $('#editor').css("font-size", fontSize);
-});
-
-$('#file-open-button').click(function(){
+$('#save-file-modal-yes').click(function(){
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, FileSystemSuccess, fail);
-    window.resolveLocalFileSystemURL("file:///storage/emulated/0/Files/file.txt", obtainFileSuccess, fail);
-});
-
-$('#saveFile').click(function(){
-    window.resolveLocalFileSystemURL("file:///storage/emulated/0/Files/", function(dir) {
+    var filename = document.getElementById('save-file-name').value
+    window.resolveLocalFileSystemURL(fileSystem.root, function(dir) {
         console.log("got main dir",dir);
-        dir.getFile("log.txt", {create:true}, function(file) {
+        dir.getFile(filename + ".txt", {create:true}, function(file) {
             console.log("got the file", file);
             logOb = file;
             var fileContent = editor.getValue();
@@ -85,7 +85,7 @@ $('#saveFile').click(function(){
 function writeLog(str) {
     if(!logOb) return;
     var log = str + " [" + (new Date()) + "]\n";
-    console.log("going to log "+log);
+    //console.log("going to log "+log);
     logOb.createWriter(function(fileWriter) {
         
         fileWriter.seek(fileWriter.length);
@@ -93,6 +93,7 @@ function writeLog(str) {
         var blob = new Blob([log], {type:'text/plain'});
         fileWriter.write(blob);
         console.log("ok, in theory i worked");
+        toast('File Saved!', 4000) // 4000 is the duration of the toast
     }, fail);
 }
 
@@ -100,21 +101,9 @@ function FileSystemSuccess(fileSystem) {
     console.log("Got the filesystem!");
 }
 
-function obtainFileSuccess(fileEntry) {
-    console.log("Obtained File!");
-    console.log(fileEntry.name);
-        fileEntry.file(function(file) {
-            var reader = new FileReader();
-            reader.onloadend = function(e) {
-                console.log("Opened File");
-                editor.setValue(this.result);
-        }
-        reader.readAsText(file);
-    });
-}
-
 function fail(e) {
     console.log("FileSystem Error");
     console.dir(e);
     console.log(e.code);
+    toast('Something Went Wrong D:', 4000) // 4000 is the duration of the toast
 }
