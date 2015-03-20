047071763ed2075c0b18b6cce17cd50541807c3b
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-03-20 22:00:09 +0000

    Testing fileSystem.root 2
    Uncommented out the requestFileSystem function and assigned the
    filesystem.root to a var

--- js/main.js
+++ js/main.js
@@ -93,12 +93,13 @@ $(document).ready(function () {
     var saveFileButton = document.getElementById("save-file-modal-yes");
 
     saveFileButton.addEventListener('click', function(e){
-        //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, FileSystemSuccess, fail);
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, FileSystemSuccess, fail);
+        var rootDir = fileSystem.root;
         console.log("Save button Clicked");
         var filename = document.getElementById('save-file-name').value;
         var dirName = document.getElementById('save-file-dir').value;
         //var directory = document.getElementById('save-file-dir').value;
-        window.resolveLocalFileSystemURL(fileSystem.root, function(dir) {
+        window.resolveLocalFileSystemURL(rootDir, function(dir) {
             console.log("got main dir",dir);
             dir.getFile(filename + ".txt", {create:true}, function(file) {
                 console.log("got the file", file);
